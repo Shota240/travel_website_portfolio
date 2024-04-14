9bf39e09114a974077f46012e7fcd2c66392cb89
Shota240 <shoup.8519@gmail.com>
2024-04-14 18:36:15 +0900

    feat :htmlをリセット、コンポーネント化開始

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30">
+
+    </nav>
+  )
+}
+
+export default Navbar
